1a9fe6b3018e69f954a802603523fe2764b6f48a
CO0K1E <74844338+CO0K1EX@users.noreply.github.com>
2023-05-02 23:18:53 +0300

    v1.0.0

---
+++ includes.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <iostream>
+#include <Windows.h>
+#include <string>
+#include <TlHelp32.h>
+#include <Shlwapi.h>
+#include <tchar.h>
+
+#include "color.hpp"
+
+#pragma comment(lib,"shlwapi.lib")
+
+#define print(format, ...) frprintf (stderr,format, __VA_ARGS__)
