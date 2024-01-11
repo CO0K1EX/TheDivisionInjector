992300bda77cfe20d618806806a34f8cabbf0655
CO0K1E <orsthosoft@gmail.com>
2024-01-11 05:48:14 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -4,7 +4,6 @@
 #include "Inject.h"
 #include "WindowLock.h"
 
-
 int main()
 {
 	SetConsoleTitleW(L"DivisionInjector");
@@ -15,52 +14,42 @@ int main()
 
 	objWindowLock.WindowSizeLock();
 
-	system("cls");
+	DWORD processGame = 0;
 
+	processGame = objProcessFinder.SearchProcess(processName);
 
 	if (!objProcessFinder.isProcessFound())
 	{
 		std::cout << "Process: " << dye::red_on_black("Not found");
-		Sleep(1000);
-		main();
+		Sleep(2500);
+		return EXIT_FAILURE;
 	}
-	else
-	{
-		std::cout << "Process: " << dye::green_on_black("Found");
-		Sleep(2000);
-	}
+
+	std::cout << "Process: " << dye::green_on_black("Found");
+	Sleep(2000);
 
 	system("cls");
 
-	std::cout << dye::yellow_on_black("DLL injector for The Division | version 1.0.0\n");
-	std::cout << "\nCreator by CO0K1E\n\n";
+	std::cout << dye::yellow_on_black("DLL injector for The Division | version 1.0.0\n\nCreator by CO0K1E\n\n");
 
 	std::string dllPath, procId;
 	std::cout << "DLL path: ";
 	std::cin >> dllPath;
 
-	DWORD processGame = 0;
-
-	processGame = objProcessFinder.SearchProcess(processName);
-
-	if (PathFileExistsA(dllPath.c_str()) == FALSE)
+	if (!PathFileExistsA(dllPath.c_str()))
 	{
-		MessageBoxExW(NULL, L"DLL file does not exist!", L"Oh uh", MB_OK | MB_ICONERROR, LANG_ENGLISH);
+		MessageBox(NULL, "DLL file does not exist!", "Oh uh", MB_OK | MB_ICONERROR);
 		return EXIT_FAILURE;
+	}
+	
+	if (objInject.InjectDLL(processGame, dllPath.c_str()) == 0)
+	{
+		MessageBox(0, "Success, game injected", "", MB_OK | MB_ICONINFORMATION);
+		return EXIT_SUCCESS;
 	}
 	else
 	{
-		objInject.InjectDLL(processGame, dllPath.c_str());
-
-		if (processGame == NULL)
-		{
-			MessageBoxExW(NULL, L"Failed to inject the game", L"Oh uh", MB_OK | MB_ICONERROR, LANG_ENGLISH);
-			return EXIT_FAILURE;
-		}
-		else
-		{
-			MessageBoxExW(NULL, L"Success, game injected", NULL, MB_OK | MB_ICONINFORMATION, LANG_ENGLISH);
-			return EXIT_SUCCESS;
-		}
+		MessageBox(0, "Failed to inject the game", "Oh uh", MB_OK | MB_ICONERROR);
+		return EXIT_FAILURE;
 	}
 }
